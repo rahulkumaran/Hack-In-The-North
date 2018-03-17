5421bfb7c0a3f2b212309f37d2f1df0a75c70eff
Saicharan Reddy <saicharan.reddy1@gmail.com>
2018-03-17 15:28:25 +0000

    Added required imports

--- actions.py
+++ actions.py
@@ -1,6 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import logging
+import warnings
+
+from policy import RestaurantPolicy
+from rasa_core import utils
 from rasa_core.actions import Action
+from rasa_core.agent import Agent
+from rasa_core.channels.console import ConsoleInputChannel
 from rasa_core.events import SlotSet
-from outcome import predict_outcome
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.policies.memoization import MemoizationPolicy
 
 #class ActionCheckRestaurants(Action):
  #  def name(self):
