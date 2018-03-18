2d9e59a277124d0e03765cea869167c0383041c5
RavicharanN <ravicharan.vsp@gmail.com>
2018-03-18 09:44:41 +0530

    Alright, manadatory changes

--- actions.py
+++ actions.py
@@ -7,7 +7,6 @@ import argparse
 import logging
 import warnings
 
-from policy import RestaurantPolicy
 from rasa_core import utils
 from rasa_core.actions import Action
 from rasa_core.agent import Agent
@@ -17,34 +16,17 @@ from rasa_core.interpreter import RasaNLUInterpreter
 from rasa_core.policies.memoization import MemoizationPolicy
 
 from outcome import predict_outcome 
-#class ActionCheckRestaurants(Action):
- #  def name(self):
-      # type: () -> Text
-  #    return "action_check_restaurants"
-
-   #def run(self, dispatcher, tracker, domain):
-      # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
-
-    #  cuisine = tracker.get_slot('cuisine')
-     # q = "select * from restaurants where cuisine='{0}' limit 1".format(cuisine)
-      #result = db.query(q)
-
-      #return [SlotSet("matches", result if result is not None else [])]
-
 
 class ActionPatientInfoRecurrence(Action):
-  
     def name(self):
         return "patient_consultation_info"
-        
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message("Getting patient depression recurrence../")
         rec_info = predict_outcome()
-        
+ 
 class ActionPatientGeneralInfo(Action):
     def name(self):
-        return "patient_general_info"
-            
+        return "patient_general_info"    
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message("Patient's general info")
         
--- outcome.py
+++ outcome.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
 pd.options.mode.chained_assignment = None
 import json
 from sklearn.externals import joblib
-import statsmodels.formula.api as smf
+# import statsmodels.formula.api as smf
 import sklearn.metrics as sm
 from math import ceil
 from sklearn import linear_model 
@@ -14,17 +14,7 @@ from sklearn import linear_model
 
 
 def predict_outcome():
-			'''
-	This function is basically used to identify and predict
-	whether or not a person might go into depression again.
-	It analyses it by looking at the age, gender,
-	time in depression before coming to meet doctor and also
-	the time for which the person stays in evaluation or the time
-	before which the person goes into depression again
-
-	Everything here is in Integers or Float64.
-			'''
-	data_input = pd.read_csv("depression.csv")		#Reading the csv file using pandas
+	data_input = pd.read_csv("depression.csv")
 	data = data_input[['Time','Age','Gender','AcuteT']]	#The machine learning model will take the following as the inputs to predict
 	
 
@@ -51,17 +41,6 @@ def predict_outcome():
 
 
 def predict_treatment():
-			'''
-	This function is basically used to identify and predict
-	what kind of treatment is to be given to a patient.
-	It analyses it by looking at the age, gender, recurrence outcome,
-	time in depression before coming to meet doctor and also
-	the time for which the person stays in evaluation or the time
-	before which the person goes into depression again
-
-	Everything here is in Integers or Float64.
-			'''
-
 	data_input = pd.read_csv("depression.csv")		#Reading the csv file using pandas
 	data_input['Treat'].replace('Lithium', 3, inplace = True)
 	data_input['Treat'].replace('Imipramine', 2, inplace = True)
