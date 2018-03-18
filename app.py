d1c1ed7339b840a6161699c2965cb6c8bb794693
RavicharanN <ravicharan.vsp@gmail.com>
2018-03-18 08:30:52 +0530

    Add messenger stuff

--- app.py
+++ app.py
@@ -3,14 +3,17 @@
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
+import json
 
 app = Flask(__name__)
-ACCESS_TOKEN = 'EAACMBXrwDvcBAAHuYk8jMxNJRMV2RnZBNZBEMs3sfckB4ytV96ZAxM62fZBc7ZBNEqy0qhFBiXya6FXZBLKVDL7LsTkraEsEzPgLZA2EQtlpIRluDiZCSpuJUKPK50HKsHIsHRX2BQyXtg9ZCsE0ha7mylr05xFGBRDjBXtOiDABglwZDZD'
+ACCESS_TOKEN = 'EAAELWqoKdy4BAKRnsbp8kZB6QivZBN46vVla9CTzEHwZCcsAAGQuyA9hLPacPXsUJGZAetuhVTO8nX6HMpEgxFfEuGimZAAGoaPvALO1GpgjrPA4VFoqToNF8HkB6c4GvGDmbS6CsKVSQY3oiz6xSZCCMsGVKX4oEcgoIsTWTSzQZDZD'
 VERIFY_TOKEN = 'VERIFY_TOKEN'
 bot = Bot(ACCESS_TOKEN)
+predefined_responses = ['Hey! Can I know your name?', 'And your age?', 'Gender?', 'How long have you been in depression?']
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
+
 def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
@@ -22,18 +25,91 @@ def receive_message():
         # get whatever message a user sent the bot
        output = request.get_json()
        for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
+        messaging = event['messaging']
+        for message in messaging:
             if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+                print(recipient_id)
+                print(message)
+                
+                json_file = open('user.json', 'r')
+                json_data = json.load(json_file)
+                json_file.close()
+                # Close after read
+                # If the user sends the message for the first time add calue to the database.
+                if recipient_id not in json_data:
+                    print("Adding", recipient_id, "to json")
+                    dump_data = {
+                        "name" : "",
+                        "age" : "",
+                        "gender" : "",
+                        "how_long" : "",
+                        "count" : 0
+                    }
+                    json_data[recipient_id] = dump_data
+                    # Write
+                    json_file = open('user.json', 'w')
+                    json.dump(json_data, json_file)
+                    json_file.close()
+                    send_message(recipient_id, predefined_responses[0])
+                    # Close after write
+                # if the instance of the user is already defined
+                else :
+                    print(recipient_id,"is already present")
+                    print(message['message']['text'])
+                    json_data[recipient_id]['count'] = json_data[recipient_id]['count'] + 1
+                    # Increment the counter and write to the file.
+                    json_file = open('user.json','w')
+                    if(json_data[recipient_id]['count'] == 1): 
+                        print("Name is", message['message']['text'])
+                        json_data[recipient_id]['name'] = message['message']['text']
+                        json.dump(json_data, json_file)
+                        json_file.close()
+                        send_message(recipient_id, predefined_responses[ json_data[recipient_id]['count'] ])
+                    elif(json_data[recipient_id]['count'] == 2):
+                        print("Age is", message['message']['text'])
+                        json_data[recipient_id]['age'] =  message['message']['text']
+                        json.dump(json_data, json_file)
+                        json_file.close()
+                        send_message(recipient_id, predefined_responses[ json_data[recipient_id]['count'] ])
+                    elif(json_data[recipient_id]['count'] == 3):
+                        print("Gender is", message['message']['text'])
+                        json_data[recipient_id]['gender'] = message['message']['text']
+                        json.dump(json_data, json_file)
+                        json_file.close()
+                        send_message(recipient_id, predefined_responses[ json_data[recipient_id]['count'] ])
+                    elif(json_data[recipient_id]['count'] == 4):
+                        print("Has been suffering since", message['message']['text'])
+                        json_data[recipient_id]['how_long'] = message['message']['text']
+                        json.dump(json_data, json_file)
+                        json_file.close()
+                        send_message(recipient_id, "It's great to know you")
+                    else:
+                        response_sent_text = get_message()
+                        json_data[recipient_id]['how_long'] = message['message']['text']
+                        json.dump(json_data, json_file)
+                        json_file.close()
+                        send_message(recipient_id, response_sent_text)
+                        # file_object = open('user.json','r')
+                        # json_data = json.load(file_object)
+                        # file_object.close()
+                        # print("The json data is",json_data)
+                        # for key in json_data:
+                        #     if json_data[key]['age'] == 'Gender?':
+                        #         del(key)
+                        # print("The  new json data is",json_data)
+                        # file_object = open('user.json','w')
+                        # json.dump(json_data, file_object)
+                        # file_object.close()
+                
+                # if message['message'].get('text'):
+                #     response_sent_text = get_message()
+                #     send_message(recipient_id, response_sent_text)
+
+        
+
+        
     return "Message Processed"
 
 def verify_fb_token(token_sent):
